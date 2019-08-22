1550c76db55196452c92cf12cb9df06a5c8b602d
dry923 <rzaleski@redhat.com>
2019-08-22 07:07:31 -0400

    Add single retry for failed tests and changed CI test execution

--- run_test.sh
+++ run_test.sh
@@ -8,27 +8,39 @@ figlet $ci_test
 
 cd $ci_dir
 
-# Apply the operator with customized namespace
-kubectl apply -f resources/namespace.yaml
+source tests/common.sh
+
+wait_clean
 
 # Test ci
 if /bin/bash tests/$ci_test.sh > $ci_test.out 2>&1
 then
   echo "$ci_dir: Successful"
-  echo "$ci_dir: Successful" >> ../ci_results
+  echo "$ci_dir: Successful" > ci_results
   echo "      <testcase classname=\"CI Results\" name=\"$ci_test\"/>" > results.xml
-  echo "$ci_test | Pass" > results.markdown
+  echo "$ci_test | Pass" > results.markdown 
 else
-  echo "$ci_dir: Failed"
-  echo "$ci_dir: Failed" >> ../ci_results
-  echo "      <testcase classname=\"CI Results\" name=\"$ci_test\" status=\"$ci_test failed\">" > results.xml
-    echo "         <failure message=\"$ci_test failure\" type=\"test failure\"/>
+  echo "$ci_dir: Failed. Retrying once"
+
+  echo "Cleaning up resources"
+  wait_clean
+
+  if /bin/bash tests/$ci_test.sh > $ci_test.out 2>&1
+  then
+    echo "$ci_dir: Successful"
+    echo "$ci_dir: Successful" > ci_results
+    echo "      <testcase classname=\"CI Results\" name=\"$ci_test\"/>" > results.xml
+    echo "$ci_test | Pass" > results.markdown
+  else
+    echo "$ci_dir: Failed second attempt" 
+    echo "$ci_dir: Failed" > ci_results
+    echo "      <testcase classname=\"CI Results\" name=\"$ci_test\" status=\"$ci_test failed\">" > results.xml
+    echo "         <failure message=\"$ci_test failed\" type=\"test failure\"/>
       </testcase>" >> results.xml
-  echo "$ci_test | Fail" > results.markdown
-  echo "Logs for "$ci_dir
-  cat $ci_test.out
+    echo "$ci_test | Fail" > results.markdown
+    echo "Logs for "$ci_dir
+    cat $ci_test.out
+  fi
 fi
 
-# Ensure that all operator resources have been cleaned up after each test as well as the namespace
-#cleanup_operator_resources
-kubectl delete -f resources/namespace.yaml
+wait_clean
--- test.sh
+++ test.sh
@@ -4,14 +4,14 @@ set -x
 source tests/common.sh
 
 cleanup_operator_resources
-update_operator_image
+#update_operator_image
 
 # Create a "gold" directory based off the current branch
 mkdir gold
 cp -pr * gold/
 
 # The maximum number of concurrent tests to run at one time (0 for unlimited)
-max_concurrent=4
+max_concurrent=1
 
 failed=()
 success=()
@@ -55,14 +55,20 @@ for ci_dir in `cat tests/my_tests`
 do
   mkdir $ci_dir
   cp -pr gold/* $ci_dir/
-  cd $ci_dir/
-  # Edit the namespaces so we can run in parallel
-  sed -i "s/my-ripsaw/my-ripsaw-$ci_dir/g" `grep -Rl my-ripsaw`
-  cd ..
 done
 
 # Run tests in parallel up to $max_concurrent at a time.
-xargs -n 1 -a tests/my_tests -P $max_concurrent ./run_test.sh
+xargs -n 1 -a tests/my_tests -P $max_concurrent ./run_test.sh 
+
+#move back to loop passing ci_dir. we can then clean and check for failure properly
+
+# Update and close JUnit test results.xml and markdown file
+for test_dir in `cat tests/my_tests`
+do
+  cat $test_dir/results.xml >> results.xml
+  cat $test_dir/results.markdown >> results.markdown
+  cat $test_dir/ci_results >> ci_results
+done
 
 # Get number of successes/failures
 testcount=`wc -l ci_results`
@@ -72,13 +78,6 @@ failcount=`grep -c Failed ci_results`
 echo "CI tests that passed: "$success
 echo "CI tests that failed: "$failed
 echo "Smoke test: Complete"
-
-# Update and close JUnit test results.xml and markdown file
-for test_dir in `cat tests/my_tests`
-do
-  cat $test_dir/results.xml >> results.xml
-  cat $test_dir/results.markdown >> results.markdown
-done
 
 echo "   </testsuite>
 </testsuites>" >> results.xml
